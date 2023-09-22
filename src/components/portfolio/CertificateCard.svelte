f463435c8d7af1f6b514d44319d02fa3ecef6392
Patato <pathonbusiness@gmail.com>
2023-09-22 22:01:41 +0700

    certificate card effects

--- src/components/portfolio/CertificateCard.svelte
+++ src/components/portfolio/CertificateCard.svelte
@@ -7,24 +7,32 @@
 
 {#if url}
 	<a href={url} target="_blank">
-		<div class="card" style="$image: url({image})">
+		<div class="card">
+			<div class="image" style="background-image: url({image})"></div>
+
+			<div class="card-content">
+				<p><strong>{name}</strong></p>
+
+				<p>{description}</p>
+			</div>
+		</div>
+	</a>
+{:else}
+	<div class="card">
+		<div class="image" style="background-image: url({image})"></div>
+
+		<div class="card-content">
 			<p><strong>{name}</strong></p>
 
 			<p>{description}</p>
 		</div>
-	</a>
-{:else}
-	<div class="card" style="$image: url({image})">
-		<p><strong>{name}</strong></p>
-
-		<p>{description}</p>
 	</div>
 {/if}
 
 <style lang="scss">
-	$image: '';
-
 	.card {
+		position: relative;
+		box-sizing: border-box;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -38,14 +46,41 @@
 		gap: 1rem;
 		aspect-ratio: 3/2;
 
-		background-position: center;
-		object-fit: cover;
-
 		text-align: center;
 
-		background-image: $image;
-		background-size: cover;
-		backdrop-filter: blur(30px);
+		&:hover {
+			.image {
+				filter: blur(0);
+			}
+
+			.card-content {
+				opacity: 0.5;
+			}
+		}
+
+		.image {
+			width: 100%;
+			height: 100%;
+
+			position: absolute;
+			z-index: 0;
+			background-image: inherit;
+			filter: blur(10px);
+
+			background-position: center;
+			object-fit: cover;
+
+			background-image: inherit;
+			background-size: cover;
+
+			transition: filter 0.3s ease;
+		}
+
+		.card-content {
+			position: absolute;
+			z-index: 1;
+			transition: opacity 0.3s ease;
+		}
 	}
 
 	a {
